9bee693a644759e43f3df69fe0bb609a45cc89af
SUBRAMANI <KADHIRVEL.SUBRAMANI@sas.dk>
2018-10-29 15:45:41 +0100

    high level component added

--- homefood-ui/src/app/app.module.ts
+++ homefood-ui/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './components/customer/login/login.component';
+import { RegisterComponent } from './components/customer/register/register.component';
+import { ChefRegisterComponent } from './components/customer/chef-register/chef-register.component';
+import { DashboardComponent } from './components/chef/dashboard/dashboard.component';
+import { HomeComponent } from './components/order/home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChefRegisterComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
