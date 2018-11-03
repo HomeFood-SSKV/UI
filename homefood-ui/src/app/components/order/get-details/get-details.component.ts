f160f2511011c26c287af3902a9be951a720bdb8
sivapgv@gmail.com <siva1234>
2018-11-03 23:49:21 +0530

    Routing implementation

--- homefood-ui/src/app/components/order/get-details/get-details.component.ts
+++ homefood-ui/src/app/components/order/get-details/get-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DeliveryAddress } from '../../../model/field-model';
 import { ApiService } from '../../../services/api.service';
+import { Router }  from '@angular/router';
 
 @Component({
   selector: 'app-get-details',
@@ -14,7 +15,8 @@ export class GetDetailsComponent implements OnInit {
   submitted = false;
   private deliveryAddress: DeliveryAddress;
   constructor(private formBuilder: FormBuilder,
-    private apiService: ApiService) { }
+    private apiService: ApiService,
+    private router: Router) { }
 
   ngOnInit() {
     this.detailsForm = this.formBuilder.group({
@@ -25,30 +27,34 @@ export class GetDetailsComponent implements OnInit {
       City: ['', Validators.required],
       PhoneNumber: ['', Validators.required],
       AddressType: ['', Validators.required]
-  });
+    });
   }
   get f() {
     return this.detailsForm.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.detailsForm.invalid) {
+      return;
+    } else if (!this.detailsForm.invalid) {
+      this.deliveryAddress = new DeliveryAddress();
+      this.deliveryAddress.fullName = this.detailsForm.value.fullName;
+      this.deliveryAddress.areaName = this.detailsForm.value.AreaName;
+      this.deliveryAddress.addressLine1 = this.detailsForm.value.AddressLine1;
+      this.deliveryAddress.addressLine2 = this.detailsForm.value.AddressLine2;
+      this.deliveryAddress.city = this.detailsForm.value.City;
+      this.deliveryAddress.phoneNumber = this.detailsForm.value.PhoneNumber;
+      this.deliveryAddress.addressType = this.detailsForm.value.AddressType;
+      const url = 'https://st1.flysddas.com/translations/sasui-upsell/upsell_en.json';
+      this.apiService.postData(url, this.deliveryAddress).subscribe((response) => {
+        console.log(response);
+        console.log('post');
+      });
     }
-    onSubmit() {
-      this.submitted = true;
-      // stop here if form is invalid
-      if (this.detailsForm.invalid) {
-          return;
-      } else if (!this.detailsForm.invalid) {
-       this.deliveryAddress = new DeliveryAddress();
-       this.deliveryAddress.fullName = this.detailsForm.value.fullName;
-       this.deliveryAddress.areaName = this.detailsForm.value.AreaName;
-       this.deliveryAddress.addressLine1 = this.detailsForm.value.AddressLine1;
-       this.deliveryAddress.addressLine2 = this.detailsForm.value.AddressLine2;
-       this.deliveryAddress.city = this.detailsForm.value.City;
-       this.deliveryAddress.phoneNumber = this.detailsForm.value.PhoneNumber;
-       this.deliveryAddress.addressType = this.detailsForm.value.AddressType;
-       const url = 'https://st1.flysddas.com/translations/sasui-upsell/upsell_en.json';
-       this.apiService.postData( url , this.deliveryAddress).subscribe((response) => {
-         console.log(response);
-         console.log('post');
-     });
-      }
+  }
+
+  public proceedToPay() {
+    this.router.navigate(['order/pay']);
   }
 }
--- homefood-ui/src/app/components/order/payment-selection/payment-selection.component.ts
+++ homefood-ui/src/app/components/order/payment-selection/payment-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }  from '@angular/router';
 
 @Component({
   selector: 'app-payment-selection',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentSelectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  public proceedToPay() {
+    this.router.navigate(['order/confirmation']);
   }
 
 }
