ee162b6fcdc4917c659b1024944baa738e3edbf4
SUBRAMANI <KADHIRVEL.SUBRAMANI@sas.dk>
2018-11-03 21:49:41 +0100

    register form validations

--- homefood-ui/src/app/components/customer/register/register.component.ts
+++ homefood-ui/src/app/components/customer/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, AbstractControl, Validators, ValidatorFn } from '@angular/forms';
 import { Register } from '../../../model/field-model';
 import { ApiService } from '../../../services/api.service';
 
@@ -11,33 +11,54 @@ import { ApiService } from '../../../services/api.service';
 })
 export class RegisterComponent implements OnInit {
 
-  registerForm: FormGroup;
-  submitted = false;
+  public registerForm: FormGroup;
+  public submitted = false;
   private register: Register;
+  private fnamePattern = '^[a-zA-Z._-]{0,100}$';
+  private mobilePattern = '^[0-9_-]{10,10}$';
+  //private emailPattern = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+  private passwordPattern = '^[a-zA-Z._-]{0,10}$';
+  private isFirstNameValid = true;
+  private isEmailIdValid = true;
+  private isMobileNumberValid = true;
+  private isPasswordValid = true;
   constructor(private formBuilder: FormBuilder,
    private apiService: ApiService) { }
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      PhoneNumber: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', [Validators.required, Validators.maxLength(100),
+        Validators.pattern(this.fnamePattern)]],
+      PhoneNumber: ['', [Validators.required, Validators.maxLength(10),
+        Validators.pattern(this.mobilePattern)]],
+      email: ['', [Validators.maxLength(100), Validators.email
+        ]],
       password: ['', [Validators.required, Validators.minLength(6)]]
   });
   }
    // convenience getter for easy access to form fields
-   get f() {
+   get _register() {
      return this.registerForm.controls;
      }
-
+     OnBlur(fieldName: string, field: any) {
+      switch (fieldName) {
+            case 'firstname':
+            const fnPat = new RegExp(this.fnamePattern);
+            this.isFirstNameValid = fnPat.test(field.value);
+            console.log(this.isFirstNameValid);
+            break;
+            case 'mobilenumber':
+            const mbPat = new RegExp(this.mobilePattern);
+            this.isMobileNumberValid = mbPat.test(field.value);
+            console.log(this.isMobileNumberValid);
+            break;
+    }
+     }
        onSubmit() {
            this.submitted = true;
-           // stop here if form is invalid
            if (this.registerForm.invalid) {
                return;
            } else if (!this.registerForm.invalid) {
-            console.log(this.registerForm.value);
-            // build request object and call API
             this.register = new Register();
             this.register.fullName = this.registerForm.value.firstName;
             this.register.phoneNumber = this.registerForm.value.phoneNumber;
@@ -48,12 +69,6 @@ export class RegisterComponent implements OnInit {
             this.apiService.postData( url , this.register).subscribe((response) => {
               console.log(response);
               console.log('post');
-          });
-
-          const geturl = 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true';
-          this.apiService.getData(geturl).subscribe((response) => {
-            console.log(response);
-            console.log('get');
           });
            }
        }
