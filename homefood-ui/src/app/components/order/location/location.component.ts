3dc30672f4f983d6a206534becbfbedb4a576f34
SUBRAMANI <KADHIRVEL.SUBRAMANI@sas.dk>
2018-11-09 23:30:14 +0100

    locations form

--- homefood-ui/src/app/components/order/location/location.component.ts
+++ homefood-ui/src/app/components/order/location/location.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl, FormGroup} from '@angular/forms';
+import { ApiService } from '../../../services/api.service';
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
 
-  constructor() { }
+    locations = [];
+    selectedLocation;
+    onSelectionChange(locations) {
+        this.selectedLocation = location;
+        // post data, if required
+    }
+    getLocations() {
+      // get delivery locations list
+      const geturl = 'https://www.googleapis.com/getlocation';
+      this.apiService.getData(geturl).subscribe((response) => {
+        console.log(response);
+        this.locations = [
+          {
+              id: 1,
+              name: 'location 1',
+              selected: false
+          },
+          {
+              id: 2,
+              name: 'location 2',
+              selected: false
+          },
+          {
+              id: 3,
+              name: 'location 3',
+              selected: false
+          }
+          ];
+      });
+    }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+   this. getLocations();
   }
 
 }
