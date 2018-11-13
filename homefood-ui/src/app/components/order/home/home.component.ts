2e0e892d35f072e03ce78b00ec650df508ef675a
sivapgv@gmail.com <siva1234>
2018-11-14 02:34:34 +0530

    added the theme

--- homefood-ui/src/app/components/order/home/home.component.ts
+++ homefood-ui/src/app/components/order/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }  from '@angular/router';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,23 @@ import { Router }  from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private cartService: CartService) { }
+  public products: any;
 
   ngOnInit() {
+    this.products = [
+      {'name': 'Veg meal', 'price':55, 'imgUrl':'', 'quantityAvailable':10},
+      {'name': 'Veg briyani', 'price':60, 'imgUrl':'', 'quantityAvailable':10},
+      {'name': 'Mutton briyani', 'price':70, 'imgUrl':'', 'quantityAvailable':10}
+  ];
+
   }
 
   public updateToCart() {
     this.router.navigate(['order/cart']);
+  }
+
+  public addProduct(index){
+    this.cartService.setCartData(this.products[index]);
   }
 }
--- homefood-ui/src/app/components/order/shopping-cart/shopping-cart.component.ts
+++ homefood-ui/src/app/components/order/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }  from '@angular/router';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,10 +8,24 @@ import { Router }  from '@angular/router';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  public cartData: Array<any> = [];
+  public totalPrice: any = 0;
+  constructor(private router: Router, private cartService: CartService) { }
 
   ngOnInit() {
+    this.cartService.getCartData().subscribe(message => {
+        this.cartData = message;
+         console.log(message); 
+         this.cartData.forEach(element => {
+          console.log(element);
+          this.totalPrice = this.totalPrice + element.price;
+        });
+      });
+    this.cartData = this.cartService.getCardDataDetails();  
+    this.cartData.forEach(element => {
+      console.log(element);
+      this.totalPrice = this.totalPrice + element.price;
+    });
   }
 
   public proceedToDetails() {
